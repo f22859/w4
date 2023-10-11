c12101385b71cca74031d65e21b3778ee3bd6c2f
Duff <144975304+f22859@users.noreply.github.com>
2023-10-11 20:06:46 +0900

    Update main.js

--- silly-story-generator/main.js
+++ silly-story-generator/main.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = '外は華氏９４度だったので、:insertx:　は買い物に行きました。　彼らが :inserty: に到着した時、　ものすごい人の多さに目を見開きました。 次の瞬間、いきなり　彼らは　:insertz:。 Bob　はその様子を見て、大爆笑しました。— :insertx: の体重は150ポンド　で、大嵐の日でした。';
+const storyText = '外は華氏９４度だったので、:insertx:　は買い物に行きました。　彼らが :inserty: に到着した時、　ものすごい人の多さに目を見開きました。 次の瞬間、いきなり　彼らは　:insertz:。 Bob　はその様子を見て、大爆笑しました。— :insertx: の体重は500ポンド　で、大嵐の日でした。';
 const insertX = ['ミッキーマウス', 'サンタクロース', 'ハーマイオニー'];
 const insertY = ['ユニバーサルスタジオ', 'エッフェル塔', '東京駅'];
 const insertZ = ['あるいていた道が陥没しました', 'ドラゴンになって飛んでいきました', '雪が降ってきて埋もれました'];
@@ -35,7 +35,7 @@ function result() {
     const weight = `${Math.round(300*0.0714286)} kg`;
     const temperature =  `${Math.round((94-32) * 5 / 9)}　　度`;
     newStory = newStory.replace('華氏９４度', temperature);
-    newStory = newStory.replace('150ポンド', weight);
+    newStory = newStory.replace('500ポンド', weight);
   }
 
   
