62f3142be5d2c05f165467ea9e074c9fa4c99307
Duff <144975304+f22859@users.noreply.github.com>
2023-10-11 18:44:30 +0900

    Update main.js

--- silly-story-generator/main.js
+++ silly-story-generator/main.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = '外は華氏　９４度だったので、:insertx:　は買い物に行きました。　彼らが :inserty: に到着した時、　ものすごい人の多さに目を見開きました。 次の瞬間、いきなり　:insertx:が　:insertz:。 Bob　はその様子を見て、大爆笑しました。— :insertx: の体重は 150　ポンド　で、大嵐の日でした。';
+const storyText = '外は華氏　９４度だったので、:insertx:　は買い物に行きました。　彼らが :inserty: に到着した時、　ものすごい人の多さに目を見開きました。 次の瞬間、いきなり　彼らは　:insertz:。 Bob　はその様子を見て、大爆笑しました。— :insertx: の体重は 150　ポンド　で、大嵐の日でした。';
 const insertX = ['ミッキーマウス', 'サンタクロース', 'ハーマイオニー'];
 const insertY = ['ユニバーサルスタジオ', 'エッフェル塔', '道端'];
 const insertZ = ['卵が爆発しました', 'ドラゴンになって飛んでいきました', '雪が降ってきて埋もれました'];
